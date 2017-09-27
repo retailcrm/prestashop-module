c6721d4563469a903042355142dcbb42dbc61461
Akolzin Dmitry <akolzinvs@gmail.com>
2017-09-27 11:29:18 +0300

    Bug fix history update

--- retailcrm/job/sync.php
+++ retailcrm/job/sync.php
@@ -366,6 +366,7 @@ if ($history->isSuccessful() && count($history->history) > 0) {
                 $api->ordersFixExternalIds($orderFix);
             }
         } else {
+            $order = $order_history;
             $orderToUpdate = new Order((int) $order['externalId']);
 
             /*
