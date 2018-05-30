f2943d6f08fe58fd740f4441a599c41042201319
Akolzin Dmitry <akolzinvs@gmail.com>
2018-05-30 10:14:25 +0300

    Update before script

--- tests/bin/before_script.sh
+++ tests/bin/before_script.sh
@@ -7,5 +7,9 @@ fi
 PRESTASHOP_DIR=$TRAVIS_BUILD_DIR/../PrestaShop
 
 cd $PRESTASHOP_DIR
-cp tests/parameters.yml.travis app/config/parameters.yml
+
+if [ -z $BRANCH ]; then
+    cp tests/parameters.yml.travis app/config/parameters.yml
+fi
+
 bash travis-scripts/install-prestashop
