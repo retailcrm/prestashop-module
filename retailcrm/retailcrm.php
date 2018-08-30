b8c77e8604e79c34250013072f8e616094ba6447
Sergey <gluschenko@retailcrm.ru>
2018-08-30 11:37:18 +0300

    v2.2.2

--- retailcrm/retailcrm.php
+++ retailcrm/retailcrm.php
@@ -3,7 +3,7 @@
  * @author Retail Driver LCC
  * @copyright RetailCRM
  * @license GPL
- * @version 2.2.0
+ * @version 2.2.2
  * @link https://retailcrm.ru
  *
  */
@@ -416,6 +416,7 @@ class RetailCRM extends Module
 
     public function hookActionOrderEdited($params)
     {
+
         $order = array(
             'externalId' => $params['order']->id,
             'firstName' => $params['customer']->firstname,
@@ -496,10 +497,8 @@ class RetailCRM extends Module
             $address = array_shift($addressCollection);
 
             if ($address instanceof Address) {
-                $phone = empty($address->phone)
-                    ? empty($address->phone_mobile) ? '' : $address->phone_mobile
-                    : $address->phone;
-
+            	$additionalPhone = !empty($address->phone) ? $address->phone : '';
+            	$phone = !empty($address->phone_mobile) ? $address->phone_mobile : '';
                 $postcode = $address->postcode;
                 $city = $address->city;
                 $addres_line = sprintf("%s %s", $address->address1, $address->address2);
@@ -520,10 +519,29 @@ class RetailCRM extends Module
                 $order['delivery']['address']['text'] = $addres_line;
             }
 
-            if (!empty($phone)) {
-                $customer['phones'][] = array('number' => $phone);
-                $order['phone'] = $phone;
-            }
+			if (!empty($phone) && !empty($additionalPhone)) {
+				$customer['phones'] = array(
+					array(
+						'number' => $phone
+					),
+					array(
+						'number' => $additionalPhone
+					)
+				);
+				$customer['phones'][] = array('number' => $phone);
+				$customer['phones'][] = array('number' => $additionalPhone);
+				$order['phone'] = $phone;
+				$order['additionalPhone'] = $additionalPhone;
+			} else {
+				$order['phone'] = !empty($phone) ? $phone : $additionalPhone;
+				$customer['phones'][] = array('number' => $order['phone']);
+			}
+
+            $comment = $params['order']->getFirstMessage();
+
+	        if ($comment !== false) {
+	            $order['customerComment'] = $comment;
+       		}
 
             foreach ($cart->getProducts() as $item) {
                 if (isset($item['id_product_attribute']) && $item['id_product_attribute'] > 0) {
