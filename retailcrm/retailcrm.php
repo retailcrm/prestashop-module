665840fac74e9bc138d328e1460666d113cbebc2
Pavel <pashok9825@gmail.com>
2019-10-29 10:56:13 +0300

    bump version

--- retailcrm/retailcrm.php
+++ retailcrm/retailcrm.php
@@ -39,7 +39,7 @@ class RetailCRM extends Module
     {
         $this->name = 'retailcrm';
         $this->tab = 'export';
-        $this->version = '2.4.0';
+        $this->version = '2.5.0';
         $this->author = 'Retail Driver LCC';
         $this->displayName = $this->l('RetailCRM');
         $this->description = $this->l('Integration module for RetailCRM');
