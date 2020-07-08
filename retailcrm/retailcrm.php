ef58da0eafb0f66828c61a99baf26b10e5ed14c3
Павел <pashok9825@gmail.com>
2020-07-08 16:29:49 +0300

    correct version

--- retailcrm/retailcrm.php
+++ retailcrm/retailcrm.php
@@ -117,7 +117,7 @@ class RetailCRM extends Module
     {
         $this->name = 'retailcrm';
         $this->tab = 'export';
-        $this->version = '4.0.0';
+        $this->version = '3.1.0';
         $this->author = 'DIGITAL RETAIL TECHNOLOGIES SL';
         $this->displayName = $this->l('retailCRM');
         $this->description = $this->l('Integration module for retailCRM');
