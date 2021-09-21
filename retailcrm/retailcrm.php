9fd39f8bb42f6cf780b3ae87de3567d92e10d60b
Almaz Bazarov <gleemand@gmail.com>
2021-09-21 11:34:00 +0300

    Bump new version of Simla.com

--- retailcrm/retailcrm.php
+++ retailcrm/retailcrm.php
@@ -143,7 +143,7 @@ class RetailCRM extends Module
     {
         $this->name = 'retailcrm';
         $this->tab = 'export';
-        $this->version = '3.3.3';
+        $this->version = '3.3.4';
         $this->author = 'DIGITAL RETAIL TECHNOLOGIES SL';
         $this->displayName = $this->l('Simla.com');
         $this->description = $this->l('Integration module for Simla.com');
