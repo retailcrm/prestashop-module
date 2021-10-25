fc482ef55e669c448226db6106a38f80d5612fd7
Apcenuu <q4nko.apcenuu@gmail.com>
2021-10-25 18:10:21 +0700

    Fix requires

--- tests/init.php
+++ tests/init.php
@@ -1,6 +1,5 @@
 <?php
 
-
-require_once __DIR__.'/config/defines.inc.php';
-require_once __DIR__.'/classes/PrestaShopAutoload.php';
+require_once __DIR__.'/../../PrestaShop/config/defines.inc.php';
+require_once __DIR__.'/../../PrestaShop/classes/PrestaShopAutoload.php';
 
