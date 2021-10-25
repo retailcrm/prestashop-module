b44645e170bc97d1d4cd2fd7ce02d7b6e3f1fcf0
Apcenuu <q4nko.apcenuu@gmail.com>
2021-10-25 18:21:44 +0700

    Fix requires

--- tests/init.php
+++ tests/init.php
@@ -3,3 +3,8 @@
 require_once __DIR__.'/../../PrestaShop/config/defines.inc.php';
 require_once __DIR__.'/../../PrestaShop/classes/PrestaShopAutoload.php';
 
+require_once __DIR__ . '/../../PrestaShop/config/config.inc.php';
+require_once __DIR__ . '/../../PrestaShop/config/defines_uri.inc.php';
+require_once __DIR__ . '/../retailcrm/bootstrap.php';
+require_once __DIR__ . '/../retailcrm/retailcrm.php';
+require_once __DIR__ . '/../../PrestaShop/init.php';
