a340a86f6e570e3f4f10c2ed5250ef5ab62386fb
Apcenuu <q4nko.apcenuu@gmail.com>
2021-10-25 19:08:10 +0700

    Fix requires

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once __DIR__ . '/tests/init.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+define('_PS_VERSION_', AppKernel::VERSION);
+
+require_once _PS_CONFIG_DIR_.'alias.php';
+require_once _PS_CLASS_DIR_.'PrestaShopAutoload.php';
+spl_autoload_register([PrestaShopAutoload::getInstance(), 'load']);
 
 
 
--- tests/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__.'/../vendor/autoload.php';
-
-define('_PS_VERSION_', AppKernel::VERSION);
-
-require_once _PS_CONFIG_DIR_.'alias.php';
-require_once _PS_CLASS_DIR_.'PrestaShopAutoload.php';
-spl_autoload_register([PrestaShopAutoload::getInstance(), 'load']);
