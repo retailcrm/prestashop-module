fb3078329d17abec98d0fc0165e159263d80555b
Apcenuu <q4nko.apcenuu@gmail.com>
2022-04-08 12:29:22 +0700

    Tests

--- tests/lib/RetailcrmHistoryTest.php
+++ tests/lib/RetailcrmHistoryTest.php
@@ -309,7 +309,7 @@ class RetailcrmHistoryTest extends RetailcrmTestCase
             unset($orderData['customer']['address']['id']);
         }
         $this->assertEquals($orderData['customer']['address'], $addressInvoice['address']);
-//        $this->assertEquals($orderData['customer']['phones'][0]['number'], $addressInvoice['phones'][0]['number']);
+        $this->assertEquals($orderData['customer']['phones'][0]['number'], $addressInvoice['phones'][0]['number']);
 
         // types and totals
         $this->assertEquals($orderData['totalSumm'], $order->total_paid);
@@ -567,9 +567,9 @@ class RetailcrmHistoryTest extends RetailcrmTestCase
         $idAddressAfter = $orderAfter->id_address_delivery;
         $addressAfter = new Address($idAddressAfter);
 
-//        if (version_compare(_PS_VERSION_, '1.7.7', '<')) {
-//            $this->assertNotEquals($idAddress, $idAddressAfter);
-//        }
+        if (version_compare(_PS_VERSION_, '1.7.7', '<')) {
+            $this->assertNotEquals($idAddress, $idAddressAfter);
+        }
 
         $this->assertEquals($crmOrder['firstName'], $addressAfter->firstname);
         $this->assertEquals($crmOrder['lastName'], $addressAfter->lastname);
